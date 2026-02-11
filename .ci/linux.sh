c2c7225141b308f46060ba46a71acc7313711b9a
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2026-02-11 04:47:03 -0800

    try this

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -78,7 +78,7 @@ CMakeNinjaBuild() {
 
 CMakeNinjaBuild SDL3
 CMakeNinjaBuild gambatte
-export EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_C_STANDARD_REQUIRED=OFF"
+export EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_C_EXTENSIONS=ON"
 CMakeNinjaBuild mgba
 
 # Install dotnet8 sdk
